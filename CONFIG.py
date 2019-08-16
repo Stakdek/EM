d44ec3ae7a22c0b7357247cabce1ee6428e85b6c
Dennis Wulfert <dennis.wulfert@perfact.de>
2019-08-16 16:18:49 +0200

    add config

---
+++ CONFIG.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+GLOBAL CONFIGURATION
+'''
+
+'''
+This telegram_token will you get if you text with the "BotFather" of Telegram.
+You will get there a Token. Paste this Token her like "TOKEN"
+'''
+telegram_token = ''
+
+
+'''
+List of urls which will be check
+'''
+urls = [
+]
